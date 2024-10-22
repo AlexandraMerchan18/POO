56747780dddd51d953f78088594512431c62861f
AlexandraMerchan18 <166462533+AlexandraMerchan18@users.noreply.github.com>
2024-10-21 21:12:09 -0500

    Update Cliente.java

--- Momento_Evaluativo2/Cliente.java
+++ Momento_Evaluativo2/Cliente.java
@@ -1,41 +1,30 @@
 public class Cliente {
     private String nombre;
-    private String cedula; // Añadimos la cédula
-    private String numeroTarjeta; // Añadimos el número de tarjeta
+    private String cedula;
 
-    public Cliente(String nombre, String cedula, String numeroTarjeta) {
+    public Cliente(String nombre, String cedula) {
         this.nombre = nombre;
         this.cedula = cedula;
-        this.numeroTarjeta = numeroTarjeta;
     }
 
-    // Getters
+    // Getters y Setters
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getCedula() {
         return cedula;
     }
 
-    public String getNumeroTarjeta() {
-        return numeroTarjeta;
-    }
-
-    // Setters
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public void setCedula(String cedula) {
         this.cedula = cedula;
     }
 
-    public void setNumeroTarjeta(String numeroTarjeta) {
-        this.numeroTarjeta = numeroTarjeta;
-    }
-
     public void mostrarInfo() {
-        System.out.println("Cliente: " + nombre + ", Cédula: " + cedula + ", Número de tarjeta: " + numeroTarjeta);
+        System.out.println("Cliente: " + nombre + ", Cédula: " + cedula);
     }
 }
