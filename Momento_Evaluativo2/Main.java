7a8d3bcce746b7f2eea7c25778a1982b4e59d60e
AlexandraMerchan18 <166462533+AlexandraMerchan18@users.noreply.github.com>
2024-10-21 21:14:53 -0500

    Update Main.java

--- Momento_Evaluativo2/Main.java
+++ Momento_Evaluativo2/Main.java
@@ -5,15 +5,17 @@ public class Main {
 
         System.out.println("\n==============================");
         // Crear un cliente
-        Cliente cliente = new Cliente("Salomón Villada Hoyos", "123456789", "987654321");
+        Cliente cliente = new Cliente("Salomón Villada Hoyos", "123456789");
 
-        // Crear productos de tipo impresión con modelo de cámara y fichero
-        Impresion impresion1 = new Impresion("Foto.png", 1, "Color", "Canon EOS R", "Camara gran angular-38mm f2.5.jpg");
-    
-        Impresion impresion2 = new Impresion("Foto.png2", 2, "Blanco y Negro", "Nikon D3500", "IMG_1099 - 26mm 1080 X 1920.jpg");
+        // Crear productos de tipo impresión
+        Impresion impresion1 = new Impresion("Foto.png", 1, "Color");
+        impresion1.agregarFoto("Camara gran angular-38mm f2.5.jpg");
+
+        Impresion impresion2 = new Impresion("Foto.png2", 2, "Blanco y Negro");
+        impresion2.agregarFoto("IMG_1099 - 26mm 1080 X 1920.jpg");
         
-        // Crear el pedido y asignar la fecha actual
-        Pedido pedido = new Pedido(cliente, new Date());
+        // Crear el pedido y asignar la fecha actual y tarjeta
+        Pedido pedido = new Pedido(cliente, new Date(), "987654321");
 
         // Agregar productos al pedido
         pedido.agregarProducto(impresion1);
@@ -22,7 +24,6 @@ public class Main {
         // Imprimir la información del pedido
         pedido.imprimirPedido();
 
-        
         System.out.println("         PothoMax         ");
         System.out.println("==============================\n");
     }
